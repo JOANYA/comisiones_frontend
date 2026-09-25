c028a7f749d46f2dfb8b62068d40bcbb3873a0ab
JOANYA <jgdiamond75@gmail.com>
2026-09-24 23:34:28 -0500

    arreglo de la url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // Cambia esto por la URL real del backend desplegado (Render, etc.)
-  apiUrl: 'https://comisiones-backend.onrender.com'
+  apiUrl: 'https://comsiones-backend.onrender.com/api'
 };
